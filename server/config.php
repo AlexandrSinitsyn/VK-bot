684e466862b29f4b65a1d81abdbae8333b6a4075
Alexandr Sinitsyn <39600558+AlexandrSinitsyn@users.noreply.github.com>
2022-10-16 15:02:29 +0300

    fix: encoding

--- server/config.php
+++ server/config.php
@@ -2,17 +2,13 @@
 	$user_id = "???";
 	
 	$app_id = 51450467;
-
-	// access token
 	
-	$standalone = "???";
+	$standalone = "???"; // access token
 	$group_token = 'e7a8b2c5a5edfe5c72e802d28035968c74552afbc11d38e1743c6fe3ec18550259d580befbb50fa220067';
 	$conf = [
 		'standalone' => $standalone,
 		'group_token' => $group_token,
 		'contorm_token' => '146da7c7',
-		'mess' => 'Фото в студио',
-		'not_command' => 'Ничего не понял!',
 		'user_id' => $user_id,
 		'group_id' => '216521057',
 		'apiurl' => 'https://api.vk.com/method/',
